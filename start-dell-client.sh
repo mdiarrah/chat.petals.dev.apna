7ca371970cd87f5b096e572c9bb113a8ae41aaf7
Mamoutou <mamoutou.diarra@hivenet.com>
2024-02-07 13:36:30 +0000

    DELL RAG with Llama 65B with docker name

--- start-dell-client.sh
+++ start-dell-client.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 sudo docker container stop dell-client-65b
 sudo docker container remove dell-client-65b
-sudo docker create -p 9090:9090 --ipc host --gpus 1 --volume petals-cache3:/root/.cache --name dell-client hive-chat-dell-65b:latest
+sudo docker create -p 9090:9090 --ipc host --gpus 1 --volume petals-cache3:/root/.cache --name dell-client-65b hive-chat-dell-65b:latest
 sudo docker container start dell-client-65b
