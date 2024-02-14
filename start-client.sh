fbbe7e110accdfdabd36e21c4b2a18bf14271f84
Mamoutou <mamoutou.diarra@hivenet.com>
2024-02-14 11:26:06 +0000

    fix hivedisk_api path

--- start-client.sh
+++ start-client.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 sudo docker container stop hive-client-llama7b
 sudo docker container remove hive-client-llama7b
-sudo docker create -p 9090:9090 --ipc host --gpus 1 --volume petals-cache3:/root/.cache --name hive-client-llama7b hive-chat-hive-llama7b:latest
+sudo docker create -p 8282:8282 --ipc host --gpus 1 --volume petals-cache3:/root/.cache --name hive-client-llama7b hive-chat-hive-llama7b:latest
 sudo docker container start hive-client-llama7b
