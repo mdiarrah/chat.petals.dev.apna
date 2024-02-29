02e1b5f45dab66741f1f6b60353c561cc858c66b
Mamoutou Diarra <121048985+mdiarrah@users.noreply.github.com>
2024-02-29 16:15:35 +0100

    Update build-dell.sh with correct petals.dev path

--- build-dell.sh
+++ build-dell.sh
@@ -3,6 +3,6 @@
 sudo docker stop dell-client-65b
 sudo docker rm -v dell-client-65b
 sudo docker image rm hive-chat-dell-65b
-cp /home/ubuntu/hiveDisk/hive-disk-python/hivedisk_api.py chat.petals.dev/
+cp /home/ubuntu/hiveDisk/hive-disk-python/hivedisk_api.py ../chat.petals.dev/
 sudo docker build . -f Dockerfile.DELL -t hive-chat-dell-65b:latest
 sh start-dell-client.sh 
