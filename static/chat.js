7fc48f20ffd50c4287559e37d3c99c1cda668fdd
Mamoutou <mamoutou.diarra@hivenet.com>
2024-04-18 08:30:22 +0200

    remove petals text

--- static/chat.js
+++ static/chat.js
@@ -63,8 +63,6 @@ function sendReplica() {
         '<span class="generation-controls"><a class="stop-generation" href=#>stop generation</a></span>' +
         '<span class="suggest-join" style="display: none;">' +
           '<b>Too slow?</b> ' +
-          '<a target="_blank" href="https://github.com/bigscience-workshop/petals#connect-your-gpu-and-increase-petals-capacity">Connect your GPU</a> ' +
-          'and increase Petals capacity!' +
         '</span>' +
       '</p>'));
     animateLoading();
