2d4f8b087d8079e1350880038875416ba479664a
Mamoutou <mamoutou.diarra@hivenet.com>
2024-04-22 13:45:11 +0200

    get INITIAl_PEERS from env variables

--- start-dell-client.sh
+++ start-dell-client.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 sudo docker container stop dell-client
 sudo docker container remove dell-client
-sudo docker create -p 8989:8989 --ipc host --gpus 1 --volume petals-cache3:/root/.cache --name dell-client hive-chat-dell:latest
+sudo docker create -p 8989:8989 --ipc host -e INITIAL_PEERS='/ip4/51.79.102.103/tcp/39337/p2p/Qma7LNTwU6MtR7CqetNuJbqWBnQ9YYQEjM6v9vQfG3bAhC' --gpus 1 --volume petals-cache3:/root/.cache --name dell-client hive-chat-dell:latest
 sudo docker container start dell-client
