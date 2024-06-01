20948ce4778ff3e2d5ba4f37bf5ebb0cce81b57d
Mamoutou <mamoutou.diarra@hivenet.com>
2024-06-01 18:00:00 +0200

    change dockerfile

--- build-apna.sh
+++ build-apna.sh
@@ -1,4 +1,4 @@
 sudo docker stop demo-client
 sudo docker rm -v demo-client
-sudo sudo docker build . -f Dockerfile.APNA -t demo-chat:latest
+sudo sudo docker build . -f Dokerfile.APNA -t demo-chat:latest
 sh start-demo-client.sh 
